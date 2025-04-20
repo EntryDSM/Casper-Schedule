95f30b5e91048f0a2cc855357aaa924d51624e03
coehgns <modooboiroo@gmail.com>
2025-04-20 23:37:06 +0900

    feat ( #1 ) : 스프링부트 프로젝트 기본 설정

---
+++ src/test/kotlin/hs/kr/entrydsm/schedule/CasperScheduleApplicationTests.kt
@@ -0,0 +1,13 @@
+package hs.kr.entrydsm.schedule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CasperScheduleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
