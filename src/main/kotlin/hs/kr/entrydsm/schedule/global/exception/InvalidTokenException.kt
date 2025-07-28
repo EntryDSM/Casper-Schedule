35efa8b5cf22ae345ef29c079d4b59eaca8a2472
coehgns <modooboiroo@gmail.com>
2025-07-28 14:01:16 +0900

    feat ( #11 ) : InvalidTokenException

--- src/main/kotlin/hs/kr/entrydsm/schedule/global/exception/InvalidTokenException.kt
+++ src/main/kotlin/hs/kr/entrydsm/schedule/global/exception/InvalidTokenException.kt
@@ -3,6 +3,10 @@ package hs.kr.entrydsm.schedule.global.exception
 import hs.kr.entrydsm.schedule.global.error.exception.CasperException
 import hs.kr.entrydsm.schedule.global.error.exception.ErrorCode
 
+/**
+ * 잘못된 토큰 예외입니다.
+ * JWT 토큰이 유효하지 않거나 형식이 맞지 않는 경우 발생합니다.
+ */
 object InvalidTokenException : CasperException(
     ErrorCode.INVALID_TOKEN
 )
