8000c222c70d110b2f8bc3efdb7d6049cc7a32fb
coehgns <modooboiroo@gmail.com>
2025-07-28 13:59:38 +0900

    feat ( #11 ) : SaveSchedulePort

---
+++ src/main/kotlin/hs/kr/entrydsm/schedule/application/schedule/port/out/SaveSchedulePort.kt
@@ -0,0 +1,16 @@
+package hs.kr.entrydsm.schedule.application.schedule.port.out
+
+import hs.kr.entrydsm.schedule.model.schedule.Schedule
+
+/**
+ * 일정을 저장하는 포트 인터페이스입니다.
+ */
+interface SaveSchedulePort {
+    /**
+     * 일정을 저장합니다.
+     *
+     * @param schedule 저장할 일정 도메인 모델
+     * @return 저장된 일정 도메인 모델
+     */
+    fun saveSchedule(schedule: Schedule): Schedule
+}
