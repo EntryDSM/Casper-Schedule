e997bfd01caf152a499ba0ab5abc86d88941b8af
coehgns <modooboiroo@gmail.com>
2025-07-28 14:01:10 +0900

    feat ( #11 ) : InternalServerErrorException

--- src/main/kotlin/hs/kr/entrydsm/schedule/global/exception/InternalServerErrorException.kt
+++ src/main/kotlin/hs/kr/entrydsm/schedule/global/exception/InternalServerErrorException.kt
@@ -3,6 +3,10 @@ package hs.kr.entrydsm.schedule.global.exception
 import hs.kr.entrydsm.schedule.global.error.exception.CasperException
 import hs.kr.entrydsm.schedule.global.error.exception.ErrorCode
 
+/**
+ * 서버 내부 오류 예외입니다.
+ * 예상치 못한 오류가 발생한 경우 사용됩니다.
+ */
 object InternalServerErrorException : CasperException(
     ErrorCode.INTERNAL_SERVER_ERROR
 )
