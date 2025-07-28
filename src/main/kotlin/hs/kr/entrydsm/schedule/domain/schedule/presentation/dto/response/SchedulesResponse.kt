0ab7b82152bf8f377532ffe276f026a2998af5b3
coehgns <modooboiroo@gmail.com>
2025-07-28 13:55:57 +0900

    feat ( #11 ) : SchedulesResponse

--- src/main/kotlin/hs/kr/entrydsm/schedule/domain/schedule/presentation/dto/response/SchedulesResponse.kt
+++
@@ -1,9 +0,0 @@
-package hs.kr.entrydsm.schedule.domain.schedule.presentation.dto.response
-
-import hs.kr.entrydsm.schedule.domain.schedule.presentation.dto.ScheduleDto
-
-data class SchedulesResponse(
-    val schedules: List<ScheduleDto?>,
-
-    val currentStatus: String
-)
