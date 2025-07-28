6f9b89f049b872bd25a8a2ecdff9d39dce2d0de4
coehgns <modooboiroo@gmail.com>
2025-07-28 14:00:00 +0900

    feat ( #11 ) : ScheduleFacade

---
+++ src/main/kotlin/hs/kr/entrydsm/schedule/facade/ScheduleFacade.kt
@@ -0,0 +1,24 @@
+package hs.kr.entrydsm.schedule.facade
+
+import hs.kr.entrydsm.schedule.application.schedule.exception.ScheduleNotFoundException
+import hs.kr.entrydsm.schedule.application.schedule.port.`in`.ScheduleFacadeUseCase
+import hs.kr.entrydsm.schedule.application.schedule.port.out.FindSchedulePort
+import hs.kr.entrydsm.schedule.model.schedule.Schedule
+import hs.kr.entrydsm.schedule.model.schedule.type.Type
+import org.springframework.stereotype.Component
+
+/**
+ * 스케줄 관련 퍼사드 클래스입니다.
+ * 애플리케이션의 외부 레이어와 도메인 로직 사이의 중재자 역할을 하며,
+ * 복잡한 도메인 로직을 캡슐화하여 간단한 인터페이스를 제공합니다.
+ *
+ * @property findSchedulePort 스케줄 조회를 위한 포트 인터페이스
+ */
+@Component
+class ScheduleFacade(
+    private val findSchedulePort: FindSchedulePort
+) : ScheduleFacadeUseCase {
+    override fun getScheduleByType(type: Type): Schedule =
+        findSchedulePort.findByType(type)
+            ?: throw ScheduleNotFoundException
+}
