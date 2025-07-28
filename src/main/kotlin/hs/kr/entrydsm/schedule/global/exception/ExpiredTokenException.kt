9c8d993de205c7708fb0a83f7a1a74b79f074a90
coehgns <modooboiroo@gmail.com>
2025-07-28 14:01:04 +0900

    feat ( #11 ) : ExpiredTokenException

--- src/main/kotlin/hs/kr/entrydsm/schedule/global/exception/ExpiredTokenException.kt
+++ src/main/kotlin/hs/kr/entrydsm/schedule/global/exception/ExpiredTokenException.kt
@@ -3,6 +3,10 @@ package hs.kr.entrydsm.schedule.global.exception
 import hs.kr.entrydsm.schedule.global.error.exception.CasperException
 import hs.kr.entrydsm.schedule.global.error.exception.ErrorCode
 
+/**
+ * 만료된 토큰 예외입니다.
+ * JWT 토큰의 유효기간이 지난 경우 발생합니다.
+ */
 object ExpiredTokenException : CasperException(
     ErrorCode.EXPIRED_TOKEN
 )
