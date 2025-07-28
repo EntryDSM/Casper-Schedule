7296dbc79a0a90c411e379f7746e2e12a24d78b9
coehgns <modooboiroo@gmail.com>
2025-07-28 14:03:52 +0900

    feat ( #11 ) : Type

--- src/main/kotlin/hs/kr/entrydsm/schedule/domain/schedule/domain/types/Type.kt
+++
@@ -1,9 +0,0 @@
-package hs.kr.entrydsm.schedule.domain.schedule.domain.types
-
-enum class Type {
-    START_DATE,
-    FIRST_ANNOUNCEMENT,
-    INTERVIEW,
-    SECOND_ANNOUNCEMENT,
-    END_DATE
-}
