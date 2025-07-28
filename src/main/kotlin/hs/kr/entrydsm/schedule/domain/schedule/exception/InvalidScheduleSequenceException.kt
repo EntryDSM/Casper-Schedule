cba56daaff1d2d27eafb96aa57c1a02f4fc2e1aa
coehgns <modooboiroo@gmail.com>
2025-07-28 13:58:37 +0900

    feat ( #11 ) : InvalidScheduleSequenceException

--- src/main/kotlin/hs/kr/entrydsm/schedule/domain/schedule/exception/InvalidScheduleSequenceException.kt
+++
@@ -1,8 +0,0 @@
-package hs.kr.entrydsm.schedule.domain.schedule.exception
-
-import hs.kr.entrydsm.schedule.global.error.exception.CasperException
-import hs.kr.entrydsm.schedule.global.error.exception.ErrorCode
-
-object InvalidScheduleSequenceException : CasperException(
-    ErrorCode.DATE_SEQUENCE_NOT_VALID
-)
