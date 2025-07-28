3783448a75d2e5a415d86d4d5bac23d1ec02f128
coehgns <modooboiroo@gmail.com>
2025-07-28 16:19:06 +0900

    docs ( #11 ) : kdoc

--- casper-schedule/src/test/kotlin/hs/kr/entrydsm/schedule/CasperScheduleApplicationTests.kt
+++ casper-schedule/src/test/kotlin/hs/kr/entrydsm/schedule/CasperScheduleApplicationTests.kt
@@ -3,9 +3,17 @@ package hs.kr.entrydsm.schedule
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+/**
+ * Casper Schedule 애플리케이션의 통합 테스트를 위한 테스트 클래스입니다.
+ * Spring Boot 애플리케이션 컨텍스트가 정상적으로 로드되는지 확인합니다.
+ */
 @SpringBootTest
 class CasperScheduleApplicationTests {
 
+	/**
+	 * 애플리케이션 컨텍스트가 정상적으로 로드되는지 테스트합니다.
+	 * Spring 애플리케이션 컨텍스트가 의존성 주입 없이도 정상적으로 시작되는지 확인합니다.
+	 */
 	@Test
 	fun contextLoads() {
 	}
