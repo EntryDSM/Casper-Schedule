3c93914f8621f8d7aa179eb954ab16a09c5aebf3
coehgns <modooboiroo@gmail.com>
2025-08-02 15:00:56 +0900

    refactor ( #11 ) : JwtProperties kdoc 수정

--- casper-schedule/src/main/kotlin/hs/kr/entrydsm/schedule/global/security/jwt/JwtProperties.kt
+++ casper-schedule/src/main/kotlin/hs/kr/entrydsm/schedule/global/security/jwt/JwtProperties.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 /**
  * JWT 관련 설정 속성을 관리하는 클래스입니다.
- * application.yml 파일에서 'auth.jwt' 하위의 설정 값을 주입받습니다.
+ * application.yml 파일에서 'jwt' 하위의 설정 값을 주입받습니다.
  *
  * @property secretKey JWT 서명에 사용되는 비밀 키
  * @property header JWT 토큰이 전달될 HTTP 헤더 이름
