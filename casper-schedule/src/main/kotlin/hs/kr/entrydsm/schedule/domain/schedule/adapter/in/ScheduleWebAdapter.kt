ad571f067e1efab3023dc444a1f1140af8458508
coehgns <modooboiroo@gmail.com>
2025-09-24 14:56:15 +0900

    feat ( #28 ) : createSchedule 메서드에 HttpStatus.CREATED 추가

--- casper-schedule/src/main/kotlin/hs/kr/entrydsm/schedule/domain/schedule/adapter/in/ScheduleWebAdapter.kt
+++ casper-schedule/src/main/kotlin/hs/kr/entrydsm/schedule/domain/schedule/adapter/in/ScheduleWebAdapter.kt
@@ -10,12 +10,14 @@ import hs.kr.entrydsm.schedule.domain.schedule.application.port.`in`.QuerySchedu
 import hs.kr.entrydsm.schedule.domain.schedule.application.port.`in`.UpdateSchedulesUseCase
 import hs.kr.entrydsm.schedule.global.document.schedule.ScheduleApiDocument
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -60,7 +62,8 @@ class ScheduleWebAdapter(
     ) = updateSchedulesUseCase.execute(request)
 
     @PostMapping
-    fun createSchedules(
+    @ResponseStatus(HttpStatus.CREATED)
+    override fun createSchedules(
         @RequestBody @Valid
         request: CreateScheduleRequest
     ) = createScheduleUseCase.execute(request)
